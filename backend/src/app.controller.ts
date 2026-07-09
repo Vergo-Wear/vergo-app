1bf02e185ca3a6ce2f1ac737476285d2e64719e2
Pamuda Uposath De Alwis Goonatilake <102458963+PamudaUposath@users.noreply.github.com>
2026-07-10 02:52:20 +0530

    Revert "feat-CU-86exy8qdu: Finalise backend database connection"

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,13 +1,9 @@
-import { Controller, Get, InternalServerErrorException } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly prisma: PrismaService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
@@ -20,23 +16,5 @@ export class AppController {
       success: true,
       message: 'Backend running successfully',
     };
-  }
-
-  @Get('/db-test')
-  async dbTest() {
-    try {
-      const result = await this.prisma.$queryRaw`SELECT 1 as test`;
-      return {
-        success: true,
-        message: 'Successfully connected to the database',
-        result,
-      };
-    } catch (error) {
-      throw new InternalServerErrorException({
-        success: false,
-        message: 'Failed to connect to the database',
-        error: error.message,
-      });
-    }
   }
 }
